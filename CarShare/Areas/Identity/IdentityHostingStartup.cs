c370122d3c855af9607b6045cd76edc9d9ae7954
Kieran Murray <s3660898@student.rmit.edu.au>
2020-03-26 12:22:27 +1100

    Added Identity pages from scaffolding

---
+++ CarShare/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CarShare.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CarShare.Areas.Identity.IdentityHostingStartup))]
+namespace CarShare.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
