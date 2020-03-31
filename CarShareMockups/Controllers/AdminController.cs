0f386b8ef2880443f01cd8cad8b4d2068afe3702
csche <csche@CHRIS-WORKPC>
2020-03-31 19:30:22 +1100

    Added user management mockup

--- CarShareMockups/Controllers/AdminController.cs
+++ CarShareMockups/Controllers/AdminController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using CarShareMockups.Models;
 using Microsoft.AspNetCore.Mvc;
+using static CarShareMockups.Models.UserViewModel;
+using CarFleetViewModel = CarShareMockups.Models.CarFleetViewModel;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -25,6 +27,17 @@ namespace CarShareMockups.Controllers
                 new CarFleetViewModel{Registration="CIH384", Make="Yellow Kia Cerato", Status=Status.Available},
                 new CarFleetViewModel{Registration="VOA927", Make="White Mitsubishi Triton", Status=Status.Suspended},
                 new CarFleetViewModel{Registration="USB308", Make="Blakc Toyota Triton", Status=Status.Suspended}
+            };
+
+            ViewBag.UserList = new List<UserViewModel>
+            {
+                new UserViewModel{Fullname="Michael Weaver", Password="M1&htyM1ke55", UserStatus=UserStatus.RentingVehicle},
+                new UserViewModel{Fullname="Jane Wong", Password="lK7>4j", UserStatus=UserStatus.Banned},
+                new UserViewModel{Fullname="Penny Green", Password="Bern@dette67", UserStatus=UserStatus.NotCurrentlyRenting},
+                new UserViewModel{Fullname="Andy Jones", Password="4t&yA9", UserStatus=UserStatus.RentingVehicle},
+                new UserViewModel{Fullname="Freddy Logan", Password="L8th<a?23Tp#", UserStatus=UserStatus.NotCurrentlyRenting},
+                new UserViewModel{Fullname="Sandy Dunn", Password="Im:)2b24", UserStatus=UserStatus.RentingVehicle},
+                new UserViewModel{Fullname="Martin West", Password="u79?jG5vDC", UserStatus=UserStatus.Banned},
             };
             return View();
         }
---
+++ CarShareMockups/Models/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShareMockups.Models
+{
+        public enum UserStatus
+        {
+            RentingVehicle,
+            NotCurrentlyRenting,
+            Banned
+        }
+
+        public class UserViewModel
+        {
+            public String Fullname { get; set; }
+            public String Password { get; set; }
+            public Enum UserStatus { get; set; }
+        }
+}
