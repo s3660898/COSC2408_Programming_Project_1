aad81126df5754ba331a70193315a7f95db3f7c4
rmit-s3765782-fatima-wali <54013430+rmit-s3765782-fatima-wali@users.noreply.github.com>
2020-04-14 09:14:28 +0300

    History controller added

---
+++ CarShare/Controllers/HistoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarShare.Data;
+using CarShare.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarShare.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class HistoryController : Controller
+    {
+
+        ApplicationDbContext _db;
+
+        public HistoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+
+    }
+}
