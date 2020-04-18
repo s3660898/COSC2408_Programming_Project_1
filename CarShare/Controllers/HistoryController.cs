29a060196c174c235c606152c82fd9f513b52589
rmit-s3765782-fatima-wali <54013430+rmit-s3765782-fatima-wali@users.noreply.github.com>
2020-04-18 13:59:13 +0300

    userhistory and carhistory are devoloped

--- CarShare/Controllers/HistoryController.cs
+++ CarShare/Controllers/HistoryController.cs
@@ -7,6 +7,8 @@ using CarShare.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using X.PagedList;
+using System.Linq;
 
 namespace CarShare.Controllers
 {
@@ -24,40 +26,67 @@ namespace CarShare.Controllers
             _logger = _logger;
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index(int carId,  int ? page = 1)
         {
-            var CarHistory = _db.CarHistory.OrderBy(a => a.Id).ToList();
+            var cars = _db.Cars.OrderBy(x => x.Id).ToList();
+           
+            const int pageSize = 50;
+            //var CarHistory = _db.CarHistory.OrderBy(a => a.Id).ToList();
 
             //_logger.LogInformation("Total record {0}", CarHistory.Count != null ? CarHistory.Count : 0);
 
-            ViewBag.CarHistoryList = new List<CarHistory>();
+            //ViewBag.CarHistoryList = new List<CarHistory>();
 
+            //foreach (CarHistory c in CarHistory)
+            //{
+            //    ViewBag.CarHistoryList.Add(c);
+            //}
+            ViewBag.carId = carId;
+            ViewBag.cars = cars;
 
-            foreach (CarHistory c in CarHistory)
+            if (carId == null)
             {
-                ViewBag.CarHistoryList.Add(c);
+                var pagedList = await _db.CarHistory.Where(a => a.CarId == 1).OrderBy(a => a.Id).ToPagedListAsync(page, pageSize);
+                return View(pagedList);
+            }
+            else {
+                var pagedList1 = await _db.CarHistory.Where(a => a.CarId == carId).OrderBy(a => a.Id).ToPagedListAsync(page, pageSize);
+                return View(pagedList1);
             }
 
-            return View();
+
+            
         }
 
 
-        public IActionResult Search()
+        public async Task<IActionResult> Search(String userId, int? page = 1)
         {
-            /*
-            var CarHistory = _db.CarHistory.OrderBy(a => a.Id).ToList();
+            const int pageSize = 5;
+            //var CarHistory = _db.CarHistory.OrderBy(a => a.Id).ToList();
 
             //_logger.LogInformation("Total record {0}", CarHistory.Count != null ? CarHistory.Count : 0);
 
-            ViewBag.CarHistoryList = new List<CarHistory>();
+            //ViewBag.CarHistoryList = new List<CarHistory>();
+
+            //foreach (CarHistory c in CarHistory)
+            //{
+            //    ViewBag.CarHistoryList.Add(c);
+            //}
 
 
-            foreach (CarHistory c in CarHistory)
+            if (userId == null)
             {
-                ViewBag.CarHistoryList.Add(c);
-            }*/
+                var pagedList = await _db.CarHistory.Where(a => a.UserId == "fbd8b270-41ef-49c5-adbd-ac324c0b1ddb").OrderBy(a => a.Id).ToPagedListAsync(page, pageSize);
+                return View(pagedList);
+            }
+            else
+            {
+                var pagedList1 = await _db.CarHistory.Where(a => a.UserId == userId).OrderBy(a => a.Id).ToPagedListAsync(page, pageSize);
+                return View(pagedList1);
+            }
 
-            return View();
+            
         }
 
 
--- CarShare/Controllers/UserhistoryController.cs
+++ CarShare/Controllers/UserhistoryController.cs
@@ -6,6 +6,7 @@ using CarShare.Data;
 using CarShare.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace CarShare.Controllers
 {
@@ -18,9 +19,35 @@ namespace CarShare.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(String userId, int? page = 1)
         {
-            return View();
+
+            const int pageSize = 50;
+            //var CarHistory = _db.CarHistory.OrderBy(a => a.Id).ToList();
+
+            //_logger.LogInformation("Total record {0}", CarHistory.Count != null ? CarHistory.Count : 0);
+
+            //ViewBag.CarHistoryList = new List<CarHistory>();
+
+            //foreach (CarHistory c in CarHistory)
+            //{
+            //    ViewBag.CarHistoryList.Add(c);
+            //}
+
+
+            ViewBag.userId = userId;
+
+            if (userId == null)
+            {
+                var pagedList = await _db.CarHistory.Where(a => a.UserId == "fbd8b270-41ef-49c5-adbd-ac324c0b1ddb-dummy").OrderBy(a => a.Id).ToPagedListAsync(page, pageSize);
+                return View(pagedList);
+            }
+            else
+            {
+                var pagedList1 = await _db.CarHistory.Where(a => a.UserId == userId).OrderBy(a => a.Id).ToPagedListAsync(page, pageSize);
+                return View(pagedList1);
+            }
+           
         }
     }
 }
