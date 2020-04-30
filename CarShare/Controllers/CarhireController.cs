0dc525094ec92eef14c20c19e8e59c699ee72a85
rmit-s3765782-fatima-wali <54013430+rmit-s3765782-fatima-wali@users.noreply.github.com>
2020-04-30 15:14:51 +0300

    Worked on car hire listing

--- CarShare/Controllers/CarhireController.cs
+++ CarShare/Controllers/CarhireController.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarShare.Data;
+using CarShare.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using X.PagedList;
 
 namespace CarShare.Controllers
 {
     public class CarhireController : Controller
     {
+        private ApplicationDbContext _db;
+
+        private ILogger<HistoryController> _logger;
+
+        public CarhireController(ApplicationDbContext db, ILogger<HistoryController> _logger)
+        {
+            _db = db;
+            _logger = _logger;
+        }
+
         public IActionResult Index()
         {
+            var cars = _db.Cars.OrderBy(x => x.Id).ToList();
+
+            ViewBag.cars = cars;
+
             return View();
         }
     }
