71fb5f8304fe3e9e6b832342ab16c3dd8ba3fc12
rmit-s3765782-fatima-wali <54013430+rmit-s3765782-fatima-wali@users.noreply.github.com>
2020-05-03 21:13:55 +0300

    working on car hire details

--- CarShare/Controllers/CarhireController.cs
+++ CarShare/Controllers/CarhireController.cs
@@ -11,6 +11,7 @@ using CarShare.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Nest;
 using X.PagedList;
 
 namespace CarShare.Controllers
@@ -30,10 +31,57 @@ namespace CarShare.Controllers
         public IActionResult Index()
         {
             var cars = _db.Cars.OrderBy(x => x.Id).ToList();
+            //37.8290° S, 144.9570° E Southebank
+            //37.8098° S, 144.9652° E central library
+            //37.7431, 145.0081 preston
+            //Console.WriteLine("Total Cars");
+
+            int totalRecord =  cars.Count();
+            Double[] Totaldistance = new Double[totalRecord]; ;
+            int counter = 0;
+            if (cars.Count() > 0) {
+                foreach (var Car in cars) {
+
+                    //Origin is harcoded
+                    var distance = DistanceTo(37.8290, 144.9570, Car.Latitude, Car.Longitude);
+                    Totaldistance[counter] = distance;
+
+                    counter++;
+                }
+            }
 
             ViewBag.cars = cars;
+            ViewBag.distance = Totaldistance;
 
             return View();
         }
+
+
+        public static double DistanceTo(double lat1, double lon1, double lat2, double lon2, char unit = 'K')
+        {
+            double rlat1 = Math.PI * lat1 / 180;
+            double rlat2 = Math.PI * lat2 / 180;
+            double theta = lon1 - lon2;
+            double rtheta = Math.PI * theta / 180;
+            double dist =
+                Math.Sin(rlat1) * Math.Sin(rlat2) + Math.Cos(rlat1) *
+                Math.Cos(rlat2) * Math.Cos(rtheta);
+            dist = Math.Acos(dist);
+            dist = dist * 180 / Math.PI;
+            dist = dist * 60 * 1.1515;
+
+            switch (unit)
+            {
+                case 'K': //Kilometers -> default
+                    return dist * 1.609344;
+                case 'N': //Nautical Miles 
+                    return dist * 0.8684;
+                case 'M': //Miles
+                    return dist;
+            }
+
+            return dist;
+        }
+
     }
 }
--- CarShare/Controllers/CarhiredetailsController.cs
+++ CarShare/Controllers/CarhiredetailsController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using X.PagedList;
 using System.Linq;
+using System.Globalization;
 
 namespace CarShare.Controllers
 {
@@ -33,9 +34,55 @@ namespace CarShare.Controllers
             return View();
         }
 
-        public IActionResult Addhire(AddCarViewModel model)
+        //public IActionResult Addhire(AddCarViewModel model)
+        public IActionResult Addhire(int id, string datepicker, string timepicker, string datepicker1, string timepicker1, string drop_off)
         {
             Console.WriteLine("FROM Car hire controller");
+            Console.WriteLine(datepicker);
+            Console.WriteLine(timepicker);
+            Console.WriteLine(datepicker1);
+            Console.WriteLine(timepicker1);
+            Console.WriteLine(drop_off);
+
+            
+            string one = datepicker;
+            string two = timepicker;
+
+            DateTime dt = Convert.ToDateTime(one + " " + two);
+            
+            Console.WriteLine(dt);
+
+            DateTime dt1 = Convert.ToDateTime(datepicker1 + " " + timepicker1);
+
+            Console.WriteLine(dt1);
+
+            TimeSpan ts = new TimeSpan(0, 2, 8);
+            string s = new DateTime(ts.Ticks).ToString("mm:ss");
+
+            CarHistory CH = new CarHistory()
+            {
+                HireTime = dt,
+                HireDuration = TimeSpan.Parse(s),
+                InitialLatitude = 37.74,
+                InitialLongitude = 145.00,
+                ReturnedLatitude = 37.74,
+                ReturnedLongitude = 145.00,
+                Status = 0,
+                ReturnedTime = dt1,
+                UserId = "fbd8b270-41ef-49c5-adbd-ac324c0b1ddb",
+                CarId = id
+            };
+
+
+            try
+            {
+                _db.CarHistory.Add(CH);
+                _db.SaveChanges();
+            } catch (Exception e) {
+                Console.WriteLine("ERROR while saving");
+            }
+
+
 
             /*
             Car c = new Car()
@@ -53,7 +100,10 @@ namespace CarShare.Controllers
 
             return RedirectToAction("FleetManagement", "Admin");
             */
-            return RedirectToAction("Carhiredetails", "Index");
+
+
+            return RedirectToAction( "Index", "Carhiredetails");
+            //return View("Index");
         }
 
 
