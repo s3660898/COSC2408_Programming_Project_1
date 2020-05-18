2db271e3efae2d4bc2e1e939ade657a8a7a5441d
Aisha Wali <54013642+rmit-s3765634-Aisha-Wali@users.noreply.github.com>
2020-05-18 18:03:07 +0300

    Add filter

--- CarShare/Controllers/CarhireController.cs
+++ CarShare/Controllers/CarhireController.cs
@@ -70,8 +70,76 @@ namespace CarShare.Controllers
         }
 
 
+        public IActionResult filter(int vehicle_type, int num_seats, String from_date, String until_date)
+        {
+            Console.WriteLine("FROM FILTER");
+            Console.WriteLine(vehicle_type);
+            Console.WriteLine(num_seats);
+            Console.WriteLine(from_date);
+            Console.WriteLine(until_date);
 
-        public static double DistanceTo(double lat1, double lon1, double lat2, double lon2, char unit = 'K')
+            var cars = _db.Cars.OrderBy(x => x.Id).ToList();
+
+            if (vehicle_type != 0) {
+                vehicle_type = vehicle_type - 1;
+
+                cars = _db.Cars.Where(x => x.Category == (CarCategory)vehicle_type).OrderBy(x => x.Id).ToList();
+
+            }
+
+            if (num_seats != 0)
+            {
+                vehicle_type = vehicle_type - 1;
+
+                cars = _db.Cars.Where(x => x.NumSeats == num_seats).OrderBy(x => x.Id).ToList();
+
+            }
+
+            int totalRecord = cars.Count();
+            Double[] Totaldistance = new Double[totalRecord];
+            String[] CarImages = new String[totalRecord];
+            String[] ImageTitle = new String[totalRecord];
+
+            int counter = 0;
+            if (cars.Count() > 0)
+            {
+                foreach (var Car in cars)
+                {
+
+                    //Origin is harcoded
+                    var distance = DistanceTo(37.8290, 144.9570, Car.Latitude, Car.Longitude);
+                    Totaldistance[counter] = distance;
+                    Console.WriteLine(Car.Latitude);
+                    Console.WriteLine(Car.Longitude);
+
+                    Image img = _db.Images.SingleOrDefault(i => i.Id == Car.ImageId);
+                    CarImages[counter] = img.Title;
+                    ImageTitle[counter] = string.Format("data:image/jgp;base64,{0}", Convert.ToBase64String(img.Data));
+
+                    counter++;
+                }
+            }
+
+            
+            ViewBag.cars = cars;
+            ViewBag.distance = Totaldistance;
+            ViewBag.imageUrl = ImageTitle;
+            ViewBag.imageTitle = CarImages;
+
+            ViewBag.vehicle_type = vehicle_type;
+            ViewBag.num_seats = num_seats;
+            ViewBag.from_date = from_date;
+            ViewBag.until_date = until_date;
+
+
+
+
+            return View();
+        }
+
+
+
+            public static double DistanceTo(double lat1, double lon1, double lat2, double lon2, char unit = 'K')
         {
             double rlat1 = Math.PI * lat1 / 180;
             double rlat2 = Math.PI * lat2 / 180;
